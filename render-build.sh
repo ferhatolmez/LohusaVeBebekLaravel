84328ab6fafc204683d2e66ce095b6d1fae28e91
ferhat ölmez <ferology1317@gmail.com>
2026-03-11 03:25:49 +0300

    Update render-build.sh to fix storage permissions

--- render-build.sh
+++ render-build.sh
@@ -18,12 +18,13 @@ mkdir -p storage/framework/sessions storage/framework/views storage/framework/ca
 mkdir -p /tmp/views
 chmod -R 777 /tmp/views
 chmod -R 777 storage/framework/cache
+chmod -R 777 storage/framework/views
 chmod -R 775 storage bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache
+chown -R www-data:www-data /tmp/views
 
 echo "Clearing old caches..."
 php artisan view:clear || true
-# php artisan cache:clear || true
 
 echo "Running migrations..."
 php artisan migrate --seed --force --no-interaction
@@ -32,9 +33,9 @@ echo "Optimizing application..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache || true
-php artisan config:clear
 
 chmod -R 777 storage/framework/views || true
+chmod -R 777 /tmp/views || true
 php artisan storage:link || true
 
 echo "Starting Apache..."
